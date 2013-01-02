3f52d4a4e484b36d87befb5bfbe8b85836e9db96
ibirrer <ibirrer@gmail.com>
2013-01-02 20:20:46 +0100

    populate games AFTER dom is loaded

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,11 @@
-$.get('razfaz.json', function(data) {
-  var template = $("article#template").remove();
-  for(i in data.games) {
-    $('#games').append(renderGame(template, data.games[i]));
-  }
+$(document).ready(function() {
+  $.get('razfaz.json', function(data) {
+    var template = $("article#template").remove();
+    for(i in data.games) {
+      $('#games').append(renderGame(template, data.games[i]));
+    }
+  });
 });
-
 
 function renderGame(tpl, game) {
   var template = tpl.clone();
