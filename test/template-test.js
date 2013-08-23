a5deef4a9d485b96384b91887d2d6d1ffc847f6e
ibirrer <ibirrer@gmail.com>
2013-08-24 01:14:09 +0200

    testing lists

--- test/template-test.js
+++ test/template-test.js
@@ -1,4 +1,4 @@
-var createFixture;
+var fixture;
 
 if (typeof require == "function" && typeof module == "object") {
   // node
@@ -6,14 +6,14 @@ if (typeof require == "function" && typeof module == "object") {
   var template = require("../lib/template");
   var cheerio = require("cheerio");
 
-  createFixture = function(fixture) {
-    $ = cheerio.load(fixture);
-    return $('div').parent();
+  fixture = function(fixture) {
+    $ = cheerio.load('<div>' + fixture + '</div>');
+    return $('div');
   }
 
 } else {
   // browser
-  createFixture = function(fixture) {
+  fixture = function(fixture) {
     return $('<div><div>' + fixture + '</div></div>').children();
   }
 }
@@ -22,14 +22,37 @@ var assert = buster.assert;
 
 buster.testCase("template", {
   "simple by class": function () {
-    var dom = createFixture('<div class="name"></div>');
+    var dom = fixture('<div class="name"></div>');
     template.render({ name: "razfaz"}, dom);
     assert.equals('<div class="name">razfaz</div>', dom.html());
   },
 
   "simple by id": function () {
-    var dom = createFixture('<div id="name"></div>');
+    var dom = fixture('<div id="name"></div>');
     template.render({ name: "razfaz"}, dom);
     assert.equals('<div id="name">razfaz</div>', dom.html());
+  },
+
+  "two occurences": function () {
+    var dom = fixture('<div class="name"></div><div class="name"></div>');
+    template.render({ name: "razfaz"}, dom);
+    assert.equals('<div class="name">razfaz</div><div class="name">razfaz</div>', dom.html());
+  },
+
+  "simple list": function () {
+    var data, dom, expected;
+    data = {
+      items: [
+      {name:"1"},
+      {name:"2"},
+      {name:"3"}]
+    };
+    expected =   '<ul><li class="items"><span class="name">1</span></li>'
+               + '<li class="items"><span class="name">2</span></li>'
+               + '<li class="items"><span class="name">3</span></li></ul>';
+
+    dom = fixture('<ul><li class="items"><span class="name"></span></li></ul>');
+    template.render(data, dom);
+    assert.equals(expected, dom.html());
   }
 })
